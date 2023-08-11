ff299e97559ad20e3fbf626c896df2971a6a0664
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-08-11 11:21:55 -0500

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,12 +2,11 @@ $(document).ready(function () {
     const nameAmenity = [];
     $('input:checkbox').click(function () {
         if ($(this).is(":checked")) {
-            nameAmenity.push($(this).attr('data_name'));
+            nameAmenity.push($(this).attr('data-name'));
         } else {
             const nameIndex = nameAmenity.indexOf($(this).attr('data-name'));
             nameAmenity.splice(nameIndex, 1);
         }
-        $('.amenities h4').text(nameAmenity.join(','));
+        $('.amenities h4').text(nameAmenity.join(', '));
     });
 });
-
